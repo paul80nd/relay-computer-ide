5aa783b44fd74cbbdbd9ea0c0bd113a7a961119c
paullaw <paul@80nd.co.uk>
2019-12-22 15:14:37 +0000

    Add monaco highlighting and completions

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -5,6 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './editor.component.html'
 })
 export class EditorComponent {
-  editorOptions = {language: 'javascript'};
-  code: string= 'function x() {\nconsole.log("Hello world!");\n}';
+  editorOptions = {language: 'rcasm', lineNumbers: 'off'};
+  code: string = ['; Demo program using MOVE8, SETAB and ALU instructions',
+  '; Evaluates ((2 + 4 + 1) x 3) - 3',
+  '',
+  'ldi b,2		; 2 + 4 = 6',
+  'ldi a,4',
+  'mov c,a',
+  'add',
+  '',
+  'mov b,a		; 6 + 1 = 7',
+  'inc',
+  '',
+  'mov b,a		; 7 * 3 = 21',
+  'mov c,a',
+  'add',			
+  'mov b,a',
+  'add',
+  '',
+  'ldi b,3		; Negate 3',
+  'not d',
+  'mov b,d',					
+  'inc d',
+  'mov b,a		; 21 - 3 = 18',
+  'mov c,d',
+  'add'].join('\n');
 }
--- src/app/components/editor/editor.module.ts
+++ src/app/components/editor/editor.module.ts
@@ -4,14 +4,83 @@ import {FormsModule} from '@angular/forms';
 
 import { EditorComponent } from './editor.component';
 
-import { MonacoEditorModule } from 'ngx-monaco-editor';
+import { MonacoEditorModule, NgxMonacoEditorConfig } from 'ngx-monaco-editor';
+import { debug, log } from 'util';
+
+interface MonarchLanguageConfiguration extends monaco.languages.IMonarchLanguage {
+  keywords: string[];
+}
+interface MonarchLanguageCompletionItemProvider extends monaco.languages.CompletionItemProvider {
+  completionMnemonics: any;
+}
+
+export function onMonacoLoad() {
+
+  monaco.languages.register({ id: 'rcasm' });
+
+  monaco.languages.setMonarchTokensProvider('rcasm', {
+    ignoreCase: true,
+    keywords: [
+      'add', 'and', 'clr', 'inc', 'mov', 'not', 'ldi', 'orr', 'eor', 'rol'
+    ],
+    types: [
+      'a', 'b', 'c', 'd'
+    ],
+    tokenizer: {        
+      root: [
+        // Identifiers and keywords
+        [/^[a-z]{3}/, {cases:{'@keywords':'keyword'}}],
+        [/[a-d]/, {cases:{'@types':'type'}}],
+
+        // Whitespace
+        { include: '@whitespace'},
+
+        // Numbers
+        [/[01]+b/,'number'],
+        [/0[xX][0-9a-fA-F]+/,'number'],
+        [/-?\d+/,'number']
+      ],
+      whitespace: [
+        [/[\t\r\n]+/,'white'],
+        [/;.*$/, 'comment'],
+      ],
+    },
+  } as MonarchLanguageConfiguration);
+
+  monaco.languages.registerCompletionItemProvider('rcasm', {
+    completionMnemonics:[
+      { label: 'add', detail: 'ALU', insertText: 'add', documentation: 'Arithmetic add (b + c) to register a (or to register d if specified).', kind: monaco.languages.CompletionItemKind.Function },                   
+      { label: 'and', detail: 'ALU', insertText: 'and', documentation: 'Binary AND (b & c) to register a (or to register d if specified).', kind: monaco.languages.CompletionItemKind.Function },                           
+      { label: 'clr', detail: 'ALU', insertText: 'clr', documentation: 'Clear register a via an ALU NO-OP (or to register d if specified).', kind: monaco.languages.CompletionItemKind.Function },                           
+      { label: 'inc', detail: 'ALU', insertText: 'inc', documentation: 'Arithmetic increment (b + 1) to register a (or to register d if specified).', kind: monaco.languages.CompletionItemKind.Function },                   
+      { label: 'not', detail: 'ALU', insertText: 'not', documentation: 'Binary NOT (~b) to register a (or to register d if specified).', kind: monaco.languages.CompletionItemKind.Function },                           
+      { label: 'orr', detail: 'ALU', insertText: 'orr', documentation: 'Binary OR (b | c) to register a (or to register d if specified).', kind: monaco.languages.CompletionItemKind.Function },                           
+      { label: 'eor', detail: 'ALU', insertText: 'eor', documentation: 'Binary exlusive OR (b ^ c) to register a (or to register d if specified).', kind: monaco.languages.CompletionItemKind.Function },   
+      { label: 'rol', detail: 'ALU', insertText: 'rol', documentation: 'Bitwise circular shift left (<<b) to register a (or to register d if specified).', kind: monaco.languages.CompletionItemKind.Function },
+      { label: 'ldi', detail: 'SETAB', insertText: 'ldi ${1:a},${2:0}', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, 
+                      documentation: 'Load immediate value to register a or b.', kind: monaco.languages.CompletionItemKind.Function },                      
+      { label: 'mov', detail: 'MOVE8', insertText: 'mov ${1:b},${2:a}', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, 
+                      documentation: 'Copy 8-bit value from register to register.', kind: monaco.languages.CompletionItemKind.Function }
+    ],
+    provideCompletionItems: function(model, position) {
+      // Provide mnemonic completions at start of line only
+      if (position.column !== 2) return { suggestions: [] };
+      return { suggestions:  this.completionMnemonics };
+    }
+  } as MonarchLanguageCompletionItemProvider);
+}
+
+const monacoConfig: NgxMonacoEditorConfig = {
+  defaultOptions: { scrollBeyondLastLine: false },
+  onMonacoLoad
+};
 
 @NgModule({
   declarations: [ EditorComponent ],
   imports:      [ 
       BrowserModule,
       FormsModule,
-      MonacoEditorModule.forRoot() ],
+      MonacoEditorModule.forRoot(monacoConfig) ],
   exports:      [ EditorComponent ]
 })
 export class EditorModule { }
