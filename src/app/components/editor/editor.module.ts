07c18023d25060ef2d505a26d52d0d0288a6d368
paullaw <paul@80nd.co.uk>
2019-12-22 19:20:55 +0000

    Change example code and update monaco providers

--- src/app/components/editor/editor.module.ts
+++ src/app/components/editor/editor.module.ts
@@ -5,16 +5,12 @@ import {FormsModule} from '@angular/forms';
 import { EditorComponent } from './editor.component';
 
 import { MonacoEditorModule, NgxMonacoEditorConfig } from 'ngx-monaco-editor';
-import { debug, log } from 'util';
 
 interface MonarchLanguageConfiguration extends monaco.languages.IMonarchLanguage {
   keywords: string[];
 }
 interface MonarchLanguageCompletionItemProvider extends monaco.languages.CompletionItemProvider {
   completionMnemonics: any;
-}
-interface MonarchLanguageSignatureHelpProvider extends monaco.languages.SignatureHelpProvider {
-  signatures: any;
 }
 
 export function onMonacoLoad() {
@@ -24,7 +20,7 @@ export function onMonacoLoad() {
   monaco.languages.setMonarchTokensProvider('rcasm', {
     ignoreCase: true,
     keywords: [
-      'add', 'and', 'clr', 'inc', 'mov', 'not', 'ldi', 'orr', 'eor', 'rol'
+      'add', 'and', 'bnz', 'clr', 'inc', 'jmp', 'mov', 'not', 'ldi', 'orr', 'eor', 'rol'
     ],
     types: [
       'a', 'b', 'c', 'd'
@@ -32,7 +28,8 @@ export function onMonacoLoad() {
     tokenizer: {
       root: [
         // Identifiers and keywords
-        [/^[a-z]{3}/, {cases: {'@keywords': 'keyword'}}],
+        [/^[a-z]+:/, 'identifier'],
+        [/[a-z]{3}/, {cases: {'@keywords': 'keyword'}}],
         [/[a-d]/, {cases: {'@types': 'type'}}],
 
         // Whitespace
@@ -52,114 +49,112 @@ export function onMonacoLoad() {
 
   monaco.languages.registerCompletionItemProvider('rcasm', {
     completionMnemonics: [
-      { label: 'add', detail: 'ALU', insertText: 'add', documentation: 'Arithmetic add (b + c) to register a (or to register d if specified).', kind: monaco.languages.CompletionItemKind.Function },
-      { label: 'and', detail: 'ALU', insertText: 'and', documentation: 'Binary AND (b & c) to register a (or to register d if specified).', kind: monaco.languages.CompletionItemKind.Function },
-      { label: 'clr', detail: 'ALU', insertText: 'clr', documentation: 'Clear register a via an ALU NO-OP (or to register d if specified).', kind: monaco.languages.CompletionItemKind.Function },
-      { label: 'inc', detail: 'ALU', insertText: 'inc', documentation: 'Arithmetic increment (b + 1) to register a (or to register d if specified).', kind: monaco.languages.CompletionItemKind.Function },
-      { label: 'not', detail: 'ALU', insertText: 'not', documentation: 'Binary NOT (~b) to register a (or to register d if specified).', kind: monaco.languages.CompletionItemKind.Function },
-      { label: 'orr', detail: 'ALU', insertText: 'orr', documentation: 'Binary OR (b | c) to register a (or to register d if specified).', kind: monaco.languages.CompletionItemKind.Function },
-      { label: 'eor', detail: 'ALU', insertText: 'eor', documentation: 'Binary exlusive OR (b ^ c) to register a (or to register d if specified).', kind: monaco.languages.CompletionItemKind.Function },
-      { label: 'rol', detail: 'ALU', insertText: 'rol', documentation: 'Bitwise circular shift left (<<b) to register a (or to register d if specified).', kind: monaco.languages.CompletionItemKind.Function },
-      { label: 'ldi', detail: 'SETAB', insertText: 'ldi ${1:a},${2:0}', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-                      documentation: 'Load immediate value to register a or b.', kind: monaco.languages.CompletionItemKind.Function },
-      { label: 'mov', detail: 'MOVE8', insertText: 'mov ${1:b},${2:a}', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-                      documentation: 'Copy 8-bit value from register to register.', kind: monaco.languages.CompletionItemKind.Function }
+      { label: 'add', detail: 'Arithmetic Add [ALU]', insertText: 'add', kind: monaco.languages.CompletionItemKind.Function },
+      { label: 'and', detail: 'Logic And [ALU]', insertText: 'and', kind: monaco.languages.CompletionItemKind.Function },
+      { label: 'clr', detail: 'Zero Value [ALU]', insertText: 'clr', kind: monaco.languages.CompletionItemKind.Function },
+      { label: 'inc', detail: 'Increment [ALU]', insertText: 'inc', kind: monaco.languages.CompletionItemKind.Function },
+      { label: 'not', detail: 'Logic Not [ALU]', insertText: 'not', kind: monaco.languages.CompletionItemKind.Function },
+      { label: 'orr', detail: 'Logic Or [ALU]', insertText: 'orr', kind: monaco.languages.CompletionItemKind.Function },
+      { label: 'eor', detail: 'Logic Xor [ALU]', insertText: 'eor', kind: monaco.languages.CompletionItemKind.Function },
+      { label: 'rol', detail: 'Bitwise Circular Shift Left [ALU]', insertText: 'rol', kind: monaco.languages.CompletionItemKind.Function },
+      { label: 'ldi', detail: 'Load Immediate [SETAB]', insertText: 'ldi ${1:a},${2:0}', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                      kind: monaco.languages.CompletionItemKind.Function },
+      { label: 'mov', detail: 'Copy Register to Register [MOV8]', insertText: 'mov ${1:b},${2:a}', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                      kind: monaco.languages.CompletionItemKind.Function },
+      { label: 'jmp', detail: 'Jump to Label [GOTO]', insertText: 'jmp', kind: monaco.languages.CompletionItemKind.Function },
+      { label: 'bnz', detail: 'Branch if Not Zero [GOTO]', insertText: 'bnz ${1:label}', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                      kind: monaco.languages.CompletionItemKind.Function }
     ],
     provideCompletionItems(model, position) {
-      // Provide mnemonic completions at start of line only
-      if (position.column !== 2) { return { suggestions: [] }; }
+      // Provide mnemonic completions following label or tabs/spaces at start of line only
+      var textOnLine = model.getValueInRange({startLineNumber: position.lineNumber, startColumn: 1, endLineNumber: position.lineNumber, endColumn: position.column});
+      var match = textOnLine.match(/^([a-z]+:)?[ \t]+/);            
+      if (!match) { return { suggestions: [] }; }
       return { suggestions:  this.completionMnemonics };
     }
   } as MonarchLanguageCompletionItemProvider);
 
-  monaco.languages.registerSignatureHelpProvider('rcasm', {
-    signatureHelpTriggerCharacters: [' '],
-   signatures: {
-     add: [{label: 'add dest', documentation: 'Arithmetic add (dest = b + c)',
-       parameters: [{label: 'dest', documentation: 'Destination register (a|d)'}]}],
-     and: [{label: 'and dest', documentation: 'Binary AND (dest = b & c)',
-       parameters: [{label: 'dest', documentation: 'Destination register (a|d)'}]}],
-     clr: [{label: 'clr dest', documentation: 'Clear (dest = 0)',
-       parameters: [{label: 'dest', documentation: 'Destination register (a|d)'}]}],
-     inc: [{label: 'inc dest', documentation: 'Arithmetic increment (dest = b + 1)',
-       parameters: [{label: 'dest', documentation: 'Destination register (a|d)'}]}],
-     ldi: [{label: 'ldi dest,value', documentation: 'Load immediate value in to dest register',
-       parameters: [{label: 'dest', documentation: 'Destination register (a|b)'},
-               {label: 'value', documentation: '5-bit sign extended value (-16 to 15)'}]}],
-     mov: [{label: 'mov dest,src', documentation: 'Copy 8-bit value from src to dest register',
-       parameters: [{label: 'dest', documentation: 'Destination register (a|b|c|d)'},
-               {label: 'src', documentation: 'Source register (a|b|c|d)'}]}],
-     not: [{label: 'not dest', documentation: 'Binary NOT (dest = ~b)',
-       parameters: [{label: 'dest', documentation: 'Destination register (a|d)'}]}],
-     orr: [{label: 'orr dest', documentation: 'Binary OR (dest = b | c)',
-       parameters: [{label: 'dest', documentation: 'Destination register (a|d)'}]}],
-     eor: [{label: 'eor dest', documentation: 'Binary exclusive OR (dest = b ^ c)',
-       parameters: [{label: 'dest', documentation: 'Destination register (a|d)'}]}],
-     rol: [{label: 'rol dest', documentation: 'Bitwise circular shift left (dest = <<b)',
-       parameters: [{label: 'dest', documentation: 'Destination register (a|d)'}]}],
-   },
-    provideSignatureHelp(model, position, token): monaco.languages.ProviderResult<monaco.languages.SignatureHelpResult> {
-     // Signature help only following mnemonic
-     if (position.column < 5) { return null; }
-     const textUntilPosition = model.getValueInRange({startLineNumber: position.lineNumber, startColumn: 1, endLineNumber: position.lineNumber, endColumn: position.column});
-     const match = textUntilPosition.match(/^([a-z]{3}) (?:([a-z0-9]*)(,([a-z0-9]*))?)?$/i);
-     if (match && match[1]) {
-       // Get signature
-       const mnemonic = match[1].toLowerCase();
-       const signatures = this.signatures[mnemonic];
-       const atParam = match[3] ? 1 : 0;
-       // Filter signatures
-       return {
-         value: {
-          activeParameter: atParam,
-          activeSignature: 0,
-          signatures: signatures.filter(function(s) { return s.parameters.length > atParam; })
-         }
-       } as monaco.languages.SignatureHelpResult;
-     }
-     return null;
-    }
- } as MonarchLanguageSignatureHelpProvider);
-
   monaco.languages.registerHoverProvider('rcasm', {
     provideHover(model, position, token): monaco.languages.ProviderResult<monaco.languages.Hover> {
       const line = model.getLineContent(position.lineNumber);
-      const match = line.match(/^([a-z]{3})(?: ([a-d])(?:,([a-d]|[01]+b|0x[0-9a-f]+|-?[0-9]+))?)?/i);
-      if (match) {
-        if (match[1]) {
-          let contents: monaco.IMarkdownString[];
-          switch (match[1].toLowerCase()) {
-            case 'add':
-              contents = [{ value: '**ALU Add (b+c)**'}, { value: 'Adds the values in register b and c, placing the result in register ' + (match[2] ? match[2] : 'a')}]; break;
-            case 'and':
-              contents = [{ value: '**ALU Binary And (b&c)**'}, { value: 'Performs a binary AND on the values in register b and c, placing the result in register ' + (match[2] ? match[2] : 'a')}]; break;
-            case 'clr':
-              contents = [{ value: '**ALU Clear**'}, { value: 'Performs a ALU clear by placing the result of a no-op in register ' + (match[2] ? match[2] : 'a')}]; break;
-            case 'inc':
-              contents = [{ value: '**ALU Increment (b+1)**'}, { value: 'Increments the value in register b, placing the result in register ' + (match[2] ? match[2] : 'a')}]; break;
-            case 'ldi':
-              if (!match[2] || !match[3]) { return null; }
-              contents = [{ value: '**Load Immediate**'}, { value: 'Loads a value of ' + match[3] + ' in to register ' + match[2]}]; break;
-            case 'mov':
-              if (!match[2] || !match[3]) { return null; }
-              contents = [{ value: '**8-bit Move**'}, { value: 'Copies the value in register ' + match[3] + ' to register ' + match[2]}]; break;
-            case 'not':
-              contents = [{ value: '**ALU Binary Not (~b)**'}, { value: 'Performs a binary NOT on the value in register b, placing the result in register ' + (match[2] ? match[2] : 'a')}]; break;
-            case 'orr':
-              contents = [{ value: '**ALU Binary Or (b|c)**'}, { value: 'Performs a binary OR on the values in register b and c, placing the result in register ' + (match[2] ? match[2] : 'a')}]; break;
-            case 'eor':
-              contents = [{ value: '**ALU Binary Exclusive Or (b^c)**'}, { value: 'Performs a binary exclusive or on the values in register b and c, placing the result in register ' + (match[2] ? match[2] : 'a')}]; break;
-            case 'rol':
-              contents = [{ value: '**ALU Bitwise Circular Shift Left (<<b)**'}, { value: 'Performs a bitwise circular shift left on the value in register b, placing the result in register ' + (match[2] ? match[2] : 'a')}]; break;
+      let contents: monaco.IMarkdownString[] = null;
+
+      // ALU match
+      const aluMatch = line.match(/^(?:[a-z]+:)?[ \t]+(add|and|clr|inc|not|orr|eor|rol)(?: ([ad]))?/i);
+      if (aluMatch && aluMatch[1])
+      {
+        let opcode = aluMatch[1];
+        let optParam = (aluMatch[2] ? aluMatch[2] : 'a').toUpperCase();
+        switch (opcode.toLowerCase()) {
+          case 'add':
+            contents = [{ value: '**Arithmetic Add [ALU]**'}, { value: '`'+optParam+' = B + C`  \nAdds the values in register B and C, placing the result in register ' + optParam}]; break;
+          case 'and':
+            contents = [{ value: '**Logical AND [ALU]**'}, { value: '`'+optParam+' = B & C`  \nPerforms a logical AND on the values in register B and C, placing the result in register ' + optParam}]; break;
+          case 'clr':
+            contents = [{ value: '**Clear Value [ALU]**'}, { value: '`'+optParam+' = 0`  \nPerforms a ALU clear by placing the result of a no-op in register ' + optParam}]; break;
+          case 'inc':
+            contents = [{ value: '**Increment [ALU]**'}, { value: '`'+optParam+' = B + 1`  \nIncrements the value in register B, placing the result in register ' + optParam}]; break;
+          case 'not':
+            contents = [{ value: '**Logical NOT [ALU]**'}, { value: '`'+optParam+' = ~B`  \nPerforms a logical NOT on the value in register B, placing the result in register ' + optParam}]; break;
+          case 'orr':
+            contents = [{ value: '**Logical OR [ALU]**'}, { value: '`'+optParam+' = B | C`  \nPerforms a logical OR on the values in register B and C, placing the result in register ' + optParam}]; break;
+          case 'eor':
+            contents = [{ value: '**Logical XOR [ALU]**'}, { value: '`'+optParam+' = B ^ C`  \nPerforms a logical exclusive or on the values in register B and C, placing the result in register ' + optParam}]; break;
+          case 'rol':
+            contents = [{ value: '**Bitwise Circular Shift Left [ALU]**'}, { value: '`'+optParam+' = <<B`  \nPerforms a bitwise circular shift left on the value in register B, placing the result in register ' + optParam}]; break;
+          default:
+            return null;
+        }
+        return {
+          range: new monaco.Range(position.lineNumber, 1, position.lineNumber, aluMatch[0].length + 1),
+          contents
+        } as monaco.languages.Hover;
+      }
+
+      // MOV match
+      const movMatch = line.match(/^(?:[a-z]+:)?[ \t]+(mov) ([a-d]),([a-d])/i);
+      if (movMatch && movMatch[1] && movMatch[2])
+      {
+        let from = movMatch[3].toUpperCase();
+        let to = movMatch[2].toUpperCase();
+        contents = [{ value: '**8-bit Move [MOV8]**'}, { value: '`'+to+' = '+ from +'`  \nCopies the value in register ' + from + ' to register ' + to}];
+        return {
+          range: new monaco.Range(position.lineNumber, 1, position.lineNumber, movMatch[0].length + 1),
+          contents
+        } as monaco.languages.Hover;
+      }
+
+      // SETAB match
+      const setMatch = line.match(/^(?:[a-z]+:)?[ \t]+(ldi) ([ab]),([01]+b|0x[0-9a-f]+|-?[0-9]+)/i)
+      if (setMatch && setMatch[1] && setMatch[2])
+      {
+        let val = setMatch[3];
+        let to = setMatch[2].toUpperCase();
+        contents = [{ value: '**Load Immediate [SETAB]**'}, { value: '`'+to+' = '+ val +'`  \nLoads a value of ' + val + ' in to register ' + to}];
+        return {
+          range: new monaco.Range(position.lineNumber, 1, position.lineNumber, setMatch[0].length + 1),
+          contents
+        } as monaco.languages.Hover;
+      }
+
+      // GOTO match
+      const gotoMatch = line.match(/^(?:[a-z]+:)?[ \t]+(bnz|jmp) ([a-z]+)/i);
+      if (gotoMatch && gotoMatch[1] && gotoMatch[2]) {
+          let label = gotoMatch[2];
+          switch (gotoMatch[1].toLowerCase()) {
+            case 'bnz':
+              contents = [{ value: '**Branch if Not Zero [GOTO]**'}, { value: 'Branches to the line labeled _' + label + '_ if the last ALU result was non-zero'}]; break;
+            case 'jmp':
+              contents = [{ value: '**Unconditional Jump [GOTO]**'}, { value: 'Unconditionally jumps to the line labeled _' + label + '_'}]; break;
             default:
               return null;
           }
           return {
-            range: new monaco.Range(position.lineNumber, 1, position.lineNumber, match[0].length + 1),
+            range: new monaco.Range(position.lineNumber, 1, position.lineNumber, gotoMatch[0].length + 1),
             contents
           } as monaco.languages.Hover;
-        }
       }
+    
       return null;
     }
   });
