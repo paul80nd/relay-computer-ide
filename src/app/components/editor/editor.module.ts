f172af6f57ace28cf3ef4537124d79f1bd6558f6
Paul Law <paul@80nd.co.uk>
2022-04-14 22:32:14 +0100

    switch ngx editor

--- src/app/components/editor/editor.module.ts
+++ src/app/components/editor/editor.module.ts
@@ -3,31 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { EditorComponent } from './editor.component';
-import { MonacoEditorModule, NgxMonacoEditorConfig } from 'ngx-monaco-editor';
-
-interface MonarchLanguageConfiguration extends monaco.languages.IMonarchLanguage {
-  keywords: string[];
-}
-interface MonarchLanguageCompletionItemProvider extends monaco.languages.CompletionItemProvider {
-  completionMnemonics: any;
-}
-
-export function onMonacoLoad() {
-
-}
-
-
-const monacoConfig: NgxMonacoEditorConfig = {
-  defaultOptions: { scrollBeyondLastLine: false },
-  onMonacoLoad
-};
+import { MonacoEditorModule } from '@materia-ui/ngx-monaco-editor';
 
 @NgModule({
   declarations: [EditorComponent],
   imports: [
     BrowserModule,
     FormsModule,
-    MonacoEditorModule.forRoot(monacoConfig)],
+    MonacoEditorModule
+  ],
   exports: [EditorComponent]
 })
 export class EditorModule { }
