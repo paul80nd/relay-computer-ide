c44ecc5a70798e8ea7a8dcab15dd57f3367fa626
Paul Law <paul@80nd.co.uk>
2022-04-16 10:55:22 +0100

    remove docs

--- src/app/components/docs/docs.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-import { DocumentationComponent } from './docs.component';
-
-@NgModule({
-  declarations: [ DocumentationComponent ],
-  exports:      [ DocumentationComponent ]
-})
-export class DocumentationModule { }
