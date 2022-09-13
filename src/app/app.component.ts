23a016a7618d4be42ea0b1b05ed13955ed0aa9a5
Paul Law <paul@80nd.co.uk>
2022-09-13 15:11:03 +0100

    add state footer to assembler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, isDevMode } from '@angular/core';
+import { Component, isDevMode, ViewChild } from '@angular/core';
+import { OutputComponent } from './output/output.component';
 import * as rcasm from '@paul80nd/rcasm';
 
 @Component({
@@ -6,8 +7,14 @@ import * as rcasm from '@paul80nd/rcasm';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+
+  @ViewChild(OutputComponent)
+  private output!: OutputComponent;
+
   dasm = ''
   isDevMode: boolean;
+  lastCompile?: Uint8Array;
+  didAssemble: boolean = false;
 
   constructor() {
     this.isDevMode = isDevMode();
@@ -15,21 +22,31 @@ export class AppComponent {
 
   onEditorCodeChanged(code: string) {
     const { prg, errors, warnings, debugInfo } = rcasm.assemble(code);
+    this.lastCompile = prg;
     if (errors.length > 0) {
-      this.dasm = '❌ Assembly Failed\n\n';
-      this.dasm += errors.map(w => `❌ ${w.loc.start.line}:${w.loc.start.column} ${w.msg}`).join('\n');
+      this.didAssemble = false;
+      this.output.setStateAssembledWithErrors(errors.length, warnings.length);
+      this.dasm = errors.map(w => `❌ ${w.loc.start.line}:${w.loc.start.column} ${w.msg}`).join('\n');
       this.dasm += '\n'
       this.dasm += warnings.map(w => `🔸 ${w.loc.start.line}:${w.loc.start.column} ${w.msg}`).join('\n');
     } else {
+      this.didAssemble = true;
       if (warnings.length > 0) {
-        this.dasm = 'Assembled OK (with warnings)\n\n';
-        this.dasm += warnings.map(w => `🔸 ${w.loc.start.line}:${w.loc.start.column} ${w.msg}`).join('\n');
+        const msg = `Assembled with ${warnings.length} warning${warnings.length === 1 ? '' : 's'}`;
+        this.output.setStateAssembledWithWarnings(warnings.length);
+        this.dasm = warnings.map(w => `🔸 ${w.loc.start.line}:${w.loc.start.column} ${w.msg}`).join('\n');
         this.dasm += '\n\n'
       } else {
-        this.dasm = 'Assembled OK\n\n';
+        this.output.setStateAssembledOk();
+        this.dasm = '';
       }
       this.dasm += rcasm.disassemble(prg, { isInstruction: debugInfo!.info().isInstruction }).join('\n');
     }
   }
 
+  exportToClipboard() {
+    if (this.didAssemble && this.lastCompile) {
+      alert([...this.lastCompile].map(x => x.toString(16).padStart(2, "0")).join(''));
+    }
+  }
 }
