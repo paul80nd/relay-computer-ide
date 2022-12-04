c79408e13fc50796feef23907c28e7d824eaaad0
Paul Law <paul@80nd.co.uk>
2022-12-04 10:24:40 +0000

    add bin pipe and hover

--- src/app/emulator/emulator.module.ts
+++ src/app/emulator/emulator.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BinPipe } from './bin.pipe';
 import { DecPipe } from './dec.pipe';
 import { HexPipe } from './hex.pipe';
 import { EmulatorComponent } from './emulator.component';
@@ -8,6 +9,7 @@ import { EmulatorComponent } from './emulator.component';
 @NgModule({
   declarations: [
     EmulatorComponent,
+    BinPipe,
     DecPipe,
     HexPipe],
   imports: [BrowserModule],
