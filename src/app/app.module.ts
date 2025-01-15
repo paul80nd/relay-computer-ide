b2e7bb2ca8edec3829992c6b414c9ce0800568d8
Paul Law <paul@80nd.co.uk>
2025-01-15 19:35:00 +0000

    upg: ng 17.3 -> 18.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -23,19 +23,17 @@ ClarityIcons.addIcons(libraryIcon, terminalIcon, importIcon, exportIcon);
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    ClarityModule,
-    DocsModule,
-    EditorModule,
-    EmulatorModule,
-    HttpClientModule,
-    OutputModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+        AppComponent
+    ],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        ClarityModule,
+        DocsModule,
+        EditorModule,
+        EmulatorModule,
+        OutputModule
+      ],
+    providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
