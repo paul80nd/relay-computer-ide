c6d7ed99743bf402efaaafb1b89e7514495aad4b
Paul Law <paul@80nd.co.uk>
2025-02-19 21:52:01 +0000

    publish

---
+++ docs/assets/monaco-editor/dev/vs/base/common/worker/simpleWorker.nls.es.js
@@ -0,0 +1,41 @@
+/*!-----------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.50.0-rc(ada2b3d6ae3e56d93f718e47f61f15f023734a14)
+ * Released under the MIT license
+ * https://github.com/microsoft/vscode/blob/main/LICENSE.txt
+ *-----------------------------------------------------------*/
+
+define("vs/base/common/worker/simpleWorker.nls.es", {
+	"vs/base/common/platform": [
+		"_",
+	],
+	"vs/editor/common/languages": [
+		"matriz",
+		"booleano",
+		"clase",
+		"constante",
+		"constructor",
+		"enumeración",
+		"miembro de la enumeración",
+		"evento",
+		"campo",
+		"archivo",
+		"función",
+		"interfaz",
+		"clave",
+		"método",
+		"módulo",
+		"espacio de nombres",
+		"NULL",
+		"número",
+		"objeto",
+		"operador",
+		"paquete",
+		"propiedad",
+		"cadena",
+		"estructura",
+		"parámetro de tipo",
+		"variable",
+		"{0} ({1})",
+	]
+});
