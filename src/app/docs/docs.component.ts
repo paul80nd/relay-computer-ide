4017e5c847e627276f3d88a5eb2e02373237bb44
Paul Law <paul@80nd.co.uk>
2025-10-27 17:19:49 +0000

    update docs

--- src/app/docs/docs.component.ts
+++ src/app/docs/docs.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ClrAccordionModule, ClrConditionalModule } from '@clr/angular';
 
 @Component({
     selector: 'app-ride-docs',
-    templateUrl: './docs.component.html'
+    templateUrl: './docs.component.html',
+    imports: [ClrAccordionModule, ClrConditionalModule]
 })
 export class DocsComponent {
 
