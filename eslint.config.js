dbf12b1b7eef949a54858fab5340f3ab2df14203
Paul Law <paul@80nd.co.uk>
2026-06-01 19:33:34 +0100

    update prettier

--- eslint.config.js
+++ eslint.config.js
@@ -9,10 +9,7 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended
-    ],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser
